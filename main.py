0dff44e473451a3736619a81a0f4d3823ab771a1
Kaan <81617346+Kaanhehe@users.noreply.github.com>
2024-02-15 15:55:13 +0100

    Made timetable display on website, with a very unique sorting and grouping type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import sqlite3
+from itertools import groupby
+from operator import itemgetter
 
 app = Flask(__name__)
 
@@ -16,7 +18,46 @@ def get_timetable_data():
 @app.route('/')
 def index():
     timetable_data = get_timetable_data()
-    return render_template('index.html', timetable_data=timetable_data)
+
+    # Replace weekdays with numbers
+    weekday_mapping = {
+        'Montag': 1,
+        'Dienstag': 2,
+        'Mittwoch': 3,
+        'Donnerstag': 4,
+        'Freitag': 5
+    }
+    """
+    Convert the timetable_data to a list of tuples and replace the weekdays with numbers
+    Sort the timetable_data by class_num and then by class_day and group the data by class_num
+    Convert the weekdays back to their original names
+    Complicated, but necessary to display the timetable in the correct order
+    !!!Did not find a better way to do this!!!
+    """
+    
+    # Convert the timetable_data to a list of tuples and replace the weekdays with numbers
+    for i in range(len(timetable_data)):
+        timetable_data[i] = list(timetable_data[i])
+        timetable_data[i][0] = weekday_mapping.get(timetable_data[i][0], timetable_data[i][0])
+        timetable_data[i] = tuple(timetable_data[i])
+
+    # Sort the timetable_data by class_num and then by class_day
+    timetable_data.sort(key=itemgetter(1, 0))  # Sort by class_num and then by class_day
+    
+    # Group the data by class_num
+    grouped_data = []
+    for key, group in groupby(timetable_data, key=itemgetter(1)): # Group by class_num
+
+        # Convert the weekdays back to their original names so 1 becomes Montag, 2 becomes Dienstag, etc.
+        group = list(group)
+        for i in range(len(group)):
+            group[i] = list(group[i])
+            group[i][0] = list(weekday_mapping.keys())[list(weekday_mapping.values()).index(group[i][0])]
+            group[i] = tuple(group[i])
+        grouped_data.append(group)
+    
+    # Render the index.html template -> templates/index.html; with the grouped_data
+    return render_template('index.html', timetable_data=grouped_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
